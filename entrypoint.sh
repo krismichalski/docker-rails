1eb647783cb3a0d5f1991848e43c884a636194bf
nooulaif <git@mail.nooulaif.com>
2016-07-17 23:31:37 +0200

    Fix entrypoint on alpine

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,17 @@ export HOME=/home/app
 echo "Starting with UID : $USER_ID"
 
 if [[ "$USER_ID" != "$(id -u app)" ]]; then
-  usermod -u $USER_ID app;
-  groupmod -g $USER_ID app;
-  chown -R $USER_ID:$USER_ID $HOME
-  usermod -g $USER_ID app;
+  if [[ "$(cat /etc/issue | grep 'Alpine')" ]]; then
+    deluser app;
+    addgroup -g $USER_ID app;
+    adduser -u $USER_ID -D -G app -s /bin/bash app;
+    chown -R app:app /home/app;
+  else
+    usermod -u $USER_ID app;
+    groupmod -g $USER_ID app;
+    chown -R $USER_ID:$USER_ID $HOME
+    usermod -g $USER_ID app;
+  fi
 fi
 
 exec /usr/local/bin/gosu app "$@"
