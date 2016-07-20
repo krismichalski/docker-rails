0bb34380559566d00d6f1b9024972a4c24f63151
nooulaif <git@mail.nooulaif.com>
2016-07-20 03:35:33 +0200

    Disable lograge in non production environments

--- template.rb
+++ template.rb
@@ -3,7 +3,8 @@ gem 'lograge', '~> 0.4.1'
 gem 'logstash-event', '~> 1.2.02'
 
 environment 'config.logger = Logger.new(STDOUT)'
-environment 'config.lograge.enabled = true'
-environment 'config.lograge.formatter = Lograge::Formatters::Logstash.new'
+
+environment 'config.lograge.enabled = true', env: :production
+environment 'config.lograge.formatter = Lograge::Formatters::Logstash.new', env: :production
 
 environment 'config.web_console.whitelisted_ips = "172.0.0.0/8" if defined?(WebConsole)', env: :development
